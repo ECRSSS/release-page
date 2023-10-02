6f52f5dbd04dd7f996c1e0e4a26829850f53c7b7
Nik <n.n.glebanov@gmail.com>
2023-10-02 22:09:10 +0300

    templates

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/index/{id}", response_class=HTMLResponse)
 async def read_item(request: Request, id: str):
-    return templates.TemplateResponse("index.html", {"request": request, "id": id})
+    return templates.TemplateResponse("index.html", {"request": request, "id": id,
+                                                     "results": [
+                                                         {"name": "Example", "status": "Accepted", "number": 34, "task_exists": False}
+                                                     ]})
