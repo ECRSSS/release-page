2a719bee426c922f844bc87aeb005389e9f92260
Nik <n.n.glebanov@gmail.com>
2023-10-02 16:06:39 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/index/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("index.html", {"request": request, "id": id})
