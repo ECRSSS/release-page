53f32e9048cd09b86a0b018f60dac017f56e7e74
nglebanov <nglebanov@mtsbank.ru>
2023-10-03 20:39:11 +0300

    versions page

---
+++ README.md
@@ -0,0 +1,31 @@
+# manual
+
+## prerequisites
+* docker
+* python 3.9 +
+* uvicorn (with python or standalone)
+
+### install uvicorn with python
+
+```shell
+pip3 install uvicorn
+```
+
+### install deps
+
+```shell
+pip3 install -r requirements.txt
+```
+
+### run mongo and server
+
+Run mongo and mongo web-interface with command
+```shell
+docker-compose up -d
+```
+
+And run server in hot-reload mode
+
+```shell
+python3 -m uvicorn main:app --reload
+```
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # This is a sample Python script.
+import requests_cache
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+from src import jira_facade
 
 app = FastAPI()
 
@@ -11,9 +14,18 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/index/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse("index.html", {"request": request, "id": id,
-                                                     "results": [
-                                                         {"name": "Example", "status": "Accepted", "number": 34, "task_exists": False}
-                                                     ]})
+class Storage:
+    FIX_VERSIONS = []
+
+
+@app.get("/", response_class=HTMLResponse)
+async def fix_versions_page(request: Request):
+    Storage.FIX_VERSIONS = jira_facade.get_versions()
+    return templates.TemplateResponse("fix_versions.html", {'request': request,
+                                                            'versions': jira_facade.get_versions()
+                                                            })
+
+
+@app.get("/", response_class=HTMLResponse)
+async def create_release_task(request: Request):
+    return templates.TemplateResponse("create_release.html", {'request': request})
